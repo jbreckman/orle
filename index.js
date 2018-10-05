9410592cd2942a5d3579082f84d2f0c87646d421
Josh Breckman IV <josh@thenumber.com>
2018-10-05 08:53:25 -0400

    Initial commit.  RLE should mostly work

---
+++ index.js
@@ -0,0 +1,119 @@
+const DATA_TYPE_LOOKUP = [ Int32Array, Int16Array, Int8Array, Uint32Array, Uint16Array, Uint8Array, Float32Array, Float64Array ];
+const TYPED_ARRAY = Int32Array.__proto__;
+
+function inferArrayType(arr) {
+  if (arr instanceof TYPED_ARRAY) {
+    return Object.getPrototypeOf(arr).constructor;
+  }
+
+  let hasNegative = false,
+      hasDecimal = false,
+      largest = 0,
+      smallest = 0;
+
+  for (var i = 0; i < arr.length; i++) {
+    let v = arr[i];
+    if (!hasNegative && v < 0) hasNegative = true;
+    if (!hasDecimal && !Number.isInteger(v)) hasDecimal = true;
+    if (v > largest) largest = v;
+    if (v < smallest) smallest = v;
+  }
+
+  if (hasDecimal) {
+    return Float64Array;
+  }
+  else if (hasNegative) {
+    if (smallest < -32768 || largest >= 32768) return Int32Array;
+    if (smallest < -128 || largest >= 128) return Int16Array;
+    return Int8Array;
+  }
+  else {
+    if (largest >= 65536) return Uint32Array;
+    if (largest >= 256) return Uint16Array;
+    return Uint8Array;
+  }
+}
+
+module.exports = {
+  encode: (arr)  => {
+    let ResultType = inferArrayType(arr);
+    
+    let result = [];
+    result.push(new Int32Array([arr.length]));
+    result.push(new Int8Array([DATA_TYPE_LOOKUP.indexOf(ResultType)]));
+
+    for (var i = 0; i < arr.length; i++) {
+      // check to see if this is a run
+      let currentValue = arr[i],
+          endRunIndex = i + 1;
+      
+      for (; endRunIndex < arr.length; endRunIndex++) {
+        if (arr[endRunIndex] !== currentValue) {
+          break;
+        }
+      }
+
+      // this is not a run, so see how many numbers are not a run
+      if (endRunIndex === i + 1) {
+        for (; endRunIndex < arr.length; endRunIndex++) {
+          if (arr[endRunIndex] === arr[endRunIndex - 1]) {
+            endRunIndex -= 1;
+            break;
+          }
+        }
+
+        result.push(new Int32Array([-(endRunIndex - i)]));
+        result.push(new ResultType([...arr.slice(i, endRunIndex)]));
+      }
+      // it is a run, so just include a count and the value
+      else {
+        result.push(new Int32Array([endRunIndex - i]));
+        result.push(new ResultType([currentValue]));
+      }
+      i = endRunIndex - 1;
+    }
+
+    return Buffer.concat(result.map(d => Buffer.from(d.buffer)));
+  },
+  decode: (buffer) => {
+    var length = buffer.readInt32LE(0),
+        ArrayType = DATA_TYPE_LOOKUP[buffer.readUInt8(4)],
+        bytesPerElement = ArrayType.BYTES_PER_ELEMENT;
+
+    buffer = buffer.slice(5);
+    var ind = 0,
+        result = new ArrayType(length);
+    
+    var resultIndex = 0;
+    while (resultIndex < length) {
+      let counter = buffer.readInt32LE(ind);
+      ind += 4;
+
+      let toRead = -counter;
+      if (counter > 0) {
+        toRead = 1;
+      }
+
+      let arr = new ArrayType(new Uint8Array(buffer.slice(ind)).buffer, 0, toRead);
+      ind += toRead * bytesPerElement;
+
+      if (counter > 0) {
+        let currentValue = arr[0],
+            endIndex = resultIndex + counter;
+        for (; resultIndex < endIndex; resultIndex++) {
+          result[resultIndex] = currentValue;
+        }
+      }
+      else {
+        let endIndex = resultIndex - counter,
+            c = 0;
+        for (; resultIndex < endIndex; resultIndex++) {
+          result[resultIndex] = arr[c++];
+        }
+      }
+
+    }    
+    
+    return result;
+  }
+};
---
+++ test.js
@@ -0,0 +1,63 @@
+const t = require('tap');
+const orle = require('./index');
+
+t.test('encode/decode', {autoend: true}, t => {
+
+  function confirm(t, arr, itemCount, itemSize, transitions) {
+    var encoded = orle.encode(arr);
+    var expectedSize = 5 + itemCount*itemSize + transitions*4;
+    t.same(encoded.length, expectedSize, 'correct size');
+    t.same([...orle.decode(encoded)], [...arr], 'correct values');
+  }
+
+  t.test('test uint8', async t => confirm(t, [1,2,3,4,5,255], 6, 1, 1));
+  t.test('test int8', async t => confirm(t, [1,2,3,4,5,-100], 6, 1, 1));
+  t.test('test uint16', async t => confirm(t, [1,2,3,4,5,256], 6, 2, 1));
+  t.test('test int16', async t => confirm(t, [1,2,3,4,5,-129], 6, 2, 1));
+  t.test('test uint32', async t => confirm(t, [1,2,3,4,5,65536], 6, 4, 1));
+  t.test('test int32', async t => confirm(t, [1,2,3,4,5,-32769], 6, 4, 1));
+  t.test('test float64', async t => confirm(t, [1,2,3,4,5,200.5], 6, 8, 1));
+  t.test('one big run', async t => confirm(t, [1,1,1,1,1,1,1], 1, 1, 1));
+  t.test('empty array', async t => confirm(t, [], 0, 0, 0));
+  t.test('one big run with odd number at end', async t => confirm(t, [1,1,1,1,1,1,2], 2, 1, 2));
+  t.test('run, mix, run', async t => confirm(t, [1,1,1,1,1,1,2,3,4,5,2,2,2,2,2,2,2], 6, 1, 3));
+  t.test('typed arrays', async t => confirm(t, new Int32Array([1,1,1,1,1,1,2,3,4,5,2,2,2,2,2,2,2]), 6, 4, 3));
+  t.test('unsigned typed arrays', async t => confirm(t, new Uint32Array([1,1,1,1,1,1,2,3,4,5,2,2,2,2,2,2,2]), 6, 4, 3));
+});
+
+t.test('performance', {autoend: true}, t => {
+
+  function buildTestData(count, countSame, countDifferent) {
+    var arr = [];
+    for (var i = 0; i < count; i++) {
+      arr.push([...new Array(countSame)].map(() => i));
+      arr.push([...new Array(countDifferent)].map(d => i + d));
+    }
+    return [].concat.apply([], arr);
+  }
+
+  function timeTestData(t, arr, maxEncodeTime, maxDecodeTime) {
+    var startTime = new Date().getTime();
+    let encoded = orle.encode(arr);
+    let duration = new Date().getTime() - startTime;
+    t.true(duration < maxEncodeTime, `faster than ${maxEncodeTime}ms to encode ${arr.length} (${duration}ms)`);
+
+    startTime = new Date().getTime(); 
+    let decoded = orle.decode(encoded);
+    duration = new Date().getTime() - startTime;
+    t.true(duration < maxDecodeTime, `faster than ${maxDecodeTime}ms to decode ${arr.length} (${duration}ms)`);
+
+    t.same(decoded.length, arr.length, 'lengths match');
+    // it's too slow to check every value, so check 10 values
+    for (var i = 1; i <= 10; i++) {
+      t.same(decoded[i*Math.floor(arr.length / 10)], arr[i*Math.floor(arr.length / 10)], `single value matches (${i})`);
+    }
+  }
+
+  t.test('large encoding/decoding', async t => timeTestData(t, buildTestData(100, 5000, 500), 150, 50));
+  t.test('large encoding/decoding known data format', async t => timeTestData(t, new Uint32Array(buildTestData(100, 5000, 500)), 100, 50));
+  t.test('very large encoding/decoding known data format', async t => timeTestData(t, new Uint32Array(buildTestData(1000, 5000, 500)), 150, 50));
+  t.test('very large mostly long runs', async t => timeTestData(t, new Uint32Array(buildTestData(100, 50000, 0)), 150, 50));
+  t.test('medium mostly long runs', async t => timeTestData(t, new Uint32Array(buildTestData(100, 500, 50)), 10, 10));
+
+});
