14b0e094f506b829560663cbe8a68a617e543ed9
Josh Breckman IV <josh@thenumber.com>
2018-10-18 13:48:59 -0400

    More timing changes

--- test.js
+++ test.js
@@ -158,8 +158,8 @@ t.test('performance', t => {
   t.test('very large encoding/decoding known data format', t => timeTestData(t, new Uint32Array(buildTestData(1000, 5000, 500)), 150, 50));
   t.test('very large mostly long runs', t => timeTestData(t, new Uint32Array(buildTestData(100, 50000, 0)), 150, 50));
   t.test('medium mostly long runs', t => timeTestData(t, new Uint32Array(buildTestData(100, 500, 50)), 30, 10));
-  t.test('pathological case', t => timeTestData(t, new Uint32Array(buildTestData(10000, 2, 3)), 100, 400));
-  t.test('one long run of strings', t => timeTestData(t, buildStringTestData(1, 0, 100000), 250, 50));
+  t.test('pathological case', t => timeTestData(t, new Uint32Array(buildTestData(10000, 2, 3)), 100, 50));
+  t.test('one long run of strings', t => timeTestData(t, buildStringTestData(1, 0, 100000), 250, 100));
   t.test('strings mixed', t => timeTestData(t, buildStringTestData(100, 500, 500), 100, 50));
   t.end();
 });
